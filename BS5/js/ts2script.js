99db107d393f3a965de6feb5345ad6cc9802a0ee
arihant-101 <121631792+arihant-101@users.noreply.github.com>
2023-09-30 23:35:41 +0530

    Delete BS5 directory

--- BS5/js/ts2script.js
+++
@@ -1,7 +0,0 @@
-function alphabet_order()
-  {
-    str="webmaster";
-    document.getElementById("var1").innerHTML=str;
-    document.getElementById("var2").innerHTML=str.split('').sort().join('');
-
-  }
